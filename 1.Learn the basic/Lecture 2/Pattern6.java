413f93fc1181f766fc0bee794bce308fc469d9af
Khushi <khushi06gupta@gmail.com>
2025-10-16 23:38:20 +0530

    Lecture 2

---
+++ 1.Learn the basic/Lecture 2/Pattern6.java
@@ -0,0 +1,12 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        int rows=5;
+        for(int i=rows-1;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
