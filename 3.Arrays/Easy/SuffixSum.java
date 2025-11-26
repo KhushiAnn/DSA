0925e693f8ae80c360c16548edf116267b745445
Khushi <khushi06gupta@gmail.com>
2025-11-26 22:03:56 +0530

    Prefix Sum and Suffix Sum

---
+++ 3.Arrays/Easy/SuffixSum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class SuffixSum {
+    public static void main(String[] args) {
+        List<Integer> arr = List.of(10, 14, 16, 20);
+
+        int n = arr.size();
+
+        // Initialize the suffix sum array with all elements as 0
+        List<Integer> suffixSum = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            suffixSum.add(0);
+        }
+
+        for (int i = 0; i < n; i++) {
+            // Calculate the sum of remaining elements to the right
+            for (int j = i; j < n; j++) {
+                suffixSum.set(i, suffixSum.get(i) + arr.get(j));
+            }
+        }
+
+        // Printing the computed suffix sum array
+        System.out.print("Suffix sum array: ");
+        for (int i = 0; i < suffixSum.size(); i++) {
+            System.out.print(suffixSum.get(i) + " ");
+        }
+    }
+}
